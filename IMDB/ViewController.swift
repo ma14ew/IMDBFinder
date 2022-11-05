2d81a97a121783477ee070b8bbf8c21fb06d4414
Matvei Matiushko <matveymatyushko@gmail.com>
2022-11-05 03:43:23 +0300

    Initial Commit

---
+++ IMDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IMDB
+//
+//  Created by Матвей Матюшко on 05.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
